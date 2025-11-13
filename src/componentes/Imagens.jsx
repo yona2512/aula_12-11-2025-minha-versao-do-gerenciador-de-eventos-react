c8d269e25969e8f50c25fad16dbc5c5d99d535c5
YONÁ HELLEN DE SOUZA <yona_souza@estudante.sesisenai.org.br>
2025-11-13 16:45:36 -0300

    atualização nova minha versão

--- src/componentes/Imagens.jsx
+++ src/componentes/Imagens.jsx
@@ -1,17 +1,26 @@
 function Imagens() {
+  const imagens = [
+    "https://www.omnieventos.com/wp-content/uploads/2018/06/eventos-corporativos.jpg",
+    "https://media.gazetadopovo.com.br/2024/03/27110327/grupo-amigos-comer-em-restaurante-960x540.jpg",
+    "https://cdn.atletis.com.br/atletis-website/base/b0a/2e6/fd6/principais-eventos-esportivos-brasil.jpg",
+    "https://www.findup.com.br/wp-content/uploads/2024/02/Prancheta-1.png"
+  ];
+
+  const imagensDuplicadas = [...imagens, ...imagens];
+
   return (
     <div className="faixa-imagens">
       <div className="container-imagens">
-        <img src="https://www.omnieventos.com/wp-content/uploads/2018/06/eventos-corporativos.jpg" alt="Imagem 1" />
-        <img src="https://media.gazetadopovo.com.br/2024/03/27110327/grupo-amigos-comer-em-restaurante-960x540.jpg" alt="Imagem 2" />
-        <img src="https://cdn.atletis.com.br/atletis-website/base/b0a/2e6/fd6/principais-eventos-esportivos-brasil.jpg" alt="Imagem 3" />
-        <img src="https://www.findup.com.br/wp-content/uploads/2024/02/Prancheta-1.png " alt="Imagem 4" />
-        <img src="https://empresasminister.com.br/wp-content/uploads/2016/07/MINISTER_header_19_7_20161.png " alt="Imagem 5" />
-        <img src="https://www.shutterstock.com/image-photo/friends-going-out-evening-making-600nw-2578716875.jpg " alt="Imagem 6" />
-        <img src="https://pt.quizur.com/_image?href=https://img.quizur.com/f/img5ded257ee2d547.10579552.jpg?lastEdited=1575822728&w=600&h=600&f=webp " alt="Imagem 7" />
-        <img src="https://www.fabrikadeeventos.com.br/wp-content/uploads/2019/11/foto-papai-noel.jpg " alt="Imagem 8" />
-        <img src="https://claudia.abril.com.br/wp-content/uploads/2025/10/halloween-festas-2.jpg?crop=1&resize=1212,909 " alt="Imagem 9" />
-        <img src="https://www.justrealmoms.com.br/wp-content/uploads/2022/04/ideias-para-pascoa-2022-just-real-moms.jpg " alt="Imagem 10" />
+        {imagensDuplicadas.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`Imagem ${index + 1}`}
+            onError={(e) => {
+              e.target.style.display = "none"; // Oculta imagem quebrada
+            }}
+          />
+        ))}
       </div>
     </div>
   );
